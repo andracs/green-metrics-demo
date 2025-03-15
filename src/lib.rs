81570364426c005556607903e797d5d98afecd62
sofush <102423577+sofush@users.noreply.github.com>
2025-03-15 23:23:52 +0100

    feat: implement wasm blake3 wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 
-#[wasm_bindgen(start)]
-fn main() {}
+#[wasm_bindgen]
+pub fn blake3(input: &str) -> String {
+    blake3::hash(input.as_bytes()).to_hex().to_string()
+}
